ab6b4d435aa04f763ddc7ac7ac6e62fe267c5a83
sPatrick2302175 <s2302175@usls.edu.ph>
2026-03-31 11:26:15 +0800

    feat(database): add migrations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,14 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            CategorySeeder::class,
+            ProductSeeder::class,
+            ProductImageSeeder::class,
+            OrderSeeder::class,
+            OrderItemSeeder::class,
+            CartItemSeeder::class,
+        ]);
     }
 }
