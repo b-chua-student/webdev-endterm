792663caf1c84d426f89a685a566b495c5d5433c
b-chua-student <s2302677@usls.edu.ph>
2026-04-02 19:44:36 +0800

    feat(routes): add root redirecting to login route
    'root' route redirects to login route to use its middlewares

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('login');
+    return redirect()->route('login');
 });
 
 Route::get('login', fn () => view('login'))
@@ -16,5 +16,6 @@ Route::post('login', [AuthController::class, 'loginByEmail'])
     ->middleware(['guest', 'throttle:10,1']);
 
 Route::post('login-guest', [AuthController::class, 'loginAsGuest'])->name('login-guest');
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('home', fn () => view('home'))->name('home');
