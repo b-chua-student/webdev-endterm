0b770cc1c2f04baf03e6bec559ba2f894952bb39
b-chua-student <s2302677@usls.edu.ph>
2026-04-02 17:56:46 +0800

    feat(routes): add guest and throttle middleware to login route
    'guest' redirects already uthenticated users to the homepage. 'throttle' only allows 10 requuests
    every 1 minute.

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ Route::get('/', function () {
     return view('login');
 });
 
-Route::post('login', [AuthControllerInterface::class, 'loginByEmail'])->name('login');
+Route::post('login', [AuthControllerInterface::class, 'loginByEmail'])
+    ->name('login')
+    ->middleware(['guest', 'throttle:10,1']);
 Route::post('login-guest', [AuthControllerInterface::class, 'loginAsGuest'])->name('login-guest');
 
 Route::get('home', fn () => view('home'))->name('home');
