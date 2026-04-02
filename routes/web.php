6ed59d327dae26f758df43cd0f12cea3b3707f7b
b-chua-student <s2302677@usls.edu.ph>
2026-04-02 18:01:47 +0800

    fix(routes): fix login route calling non-existing controller
    renamed AuthControllerInterface to AuthController

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('login');
 });
 
-Route::post('login', [AuthControllerInterface::class, 'loginByEmail'])
+Route::post('login', [AuthController::class, 'loginByEmail'])
     ->name('login')
     ->middleware(['guest', 'throttle:10,1']);
-Route::post('login-guest', [AuthControllerInterface::class, 'loginAsGuest'])->name('login-guest');
+Route::post('login-guest', [AuthController::class, 'loginAsGuest'])->name('login-guest');
 
 Route::get('home', fn () => view('home'))->name('home');
