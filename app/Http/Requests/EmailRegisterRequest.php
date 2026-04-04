8873c2a686f4f4be137a8c712a68f20dea146c16
b-chua-student <s2302677@usls.edu.ph>
2026-04-05 04:09:52 +0800

    feat(form request): add email register form request

---
+++ app/Http/Requests/EmailRegisterRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmailRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name'       => 'required',
+            'last_name'        => 'required',
+            'email'            => 'required|email|not_in:guest@guest|unique:users,email',
+            'password'         => 'required|min:8|confirmed',
+        ];
+    }
+}
