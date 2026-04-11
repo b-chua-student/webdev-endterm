69e0001337d0004e6a6c9179d8dd1f3072e17ac5
b-chua-student <s2302677@usls.edu.ph>
2026-04-11 15:43:06 +0800

    refactor/organize views (#16)
    * Squashed commit of the following:
    
    commit 37e272718eb85668ce57fd3aadc6cdfdbd084831
    Author: b-chua-student <s2302677@usls.edu.ph>
    Date:   Sat Apr 11 09:19:29 2026 +0800
    
        feat/add user and guest login tests (#12)
    
        * test(auth service): test login function handling valid and invalid credentials
    
        * test(login): add feature tests for whole login functionality
    
    * feat(layouts): add new layouts for user and auth pages
    
    * refactor(views): put test views ina dedicated test directory
    
    * fix(views): delete unused welcome view
    
    * fix(routes): changed get routes to point to test views

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>@yield('title')</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="description" content="" />
+  <link rel="icon" href="favicon.png">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('css/components/style.css') }}">
+  @vite(['resources/css/app.css'])
+</head>
+<body>
+
+    @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" />
+</body>
+</html>
+
