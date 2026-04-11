2478678b8837b4974a7c6dd1f89fd8f2a7dffed8
b-chua-student <s2302677@usls.edu.ph>
2026-04-11 16:01:04 +0800

    feat(view): add product listing view

---
+++ resources/views/product-listing.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.user')
+
+@section('title', 'Product Listing')
+
+@section('content')
+
+<x-side-navbar />
+<x-product-listing-card />
+<x-product-listing-card />
+<x-product-listing-card />
+<x-product-listing-card />
+<x-product-listing-card />
+<x-product-listing-card />
+
+@endsection
