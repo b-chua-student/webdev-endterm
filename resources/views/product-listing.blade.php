a6ce45366d9048f65bdda66b42f85a6b88579a7d
Glen <s2202765@usls.edu.ph>
2026-04-14 12:48:02 +0800

    feat: changed product-listing.blade.php, and side-navbar.blade.php

--- resources/views/product-listing.blade.php
+++ resources/views/product-listing.blade.php
@@ -1,15 +1,23 @@
 @extends('layouts.user')
 
-@section('title', 'Product Listing')
-
 @section('content')
+<div class="d-flex w-100 min-vh-100">
+    
+    <aside class="flex-shrink-0" style="width: 280px; background-color: #800000; border-top: 1px solid rgba(255,255,255,0.2);">
+        <x-side-navbar />
+    </aside>
 
-<x-side-navbar />
-<x-product-listing-card />
-<x-product-listing-card />
-<x-product-listing-card />
-<x-product-listing-card />
-<x-product-listing-card />
-<x-product-listing-card />
+    <main class="flex-grow-1 p-5 bg-white">
+        <div class="container-fluid">
+            <div class="row row-cols-1 row-cols-md-3 g-5">
+                @for ($i = 0; $i < 6; $i++)
+                    <div class="col">
+                        <x-product-listing-card />
+                    </div>
+                @endfor
+            </div>
+        </div>
+    </main>
 
+</div>
 @endsection
