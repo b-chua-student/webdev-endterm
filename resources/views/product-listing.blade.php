47b58b61f5325edc31c87df8c3e9d51ced6768b2
b-chua-student <s2302677@usls.edu.ph>
2026-04-16 02:00:36 +0800

    fix(view): fixed overlapping on 16:10 screens

--- resources/views/product-listing.blade.php
+++ resources/views/product-listing.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 <div class="d-flex w-100 min-vh-100">
-    
+
     <aside class="flex-shrink-0" style="width: 280px; background-color: #800000; border-top: 1px solid rgba(255,255,255,0.2);">
         <x-side-navbar />
     </aside>
 
     <main class="flex-grow-1 p-5 bg-white">
         <div class="container-fluid">
-            <div class="row row-cols-1 row-cols-md-3 g-5">
+            <div class="row g-5">
                 @for ($i = 0; $i < 6; $i++)
-                    <div class="col">
+                    <div class="col-auto">
                         <x-product-listing-card />
                     </div>
                 @endfor
