bf331ac43a3ec98be42d1221f3abbc59f676d1fd
b-chua-student <s2302677@usls.edu.ph>
2026-04-19 09:00:21 +0800

    feat(tag component): add toggleable tag component and styling

---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,4 @@
+<div>
+  <input type="checkbox" class="btn-check" id="tag-toggle-{{ $tagName }}" autocomplete="off">
+  <label class="btn btn-outline-primary tag-btn-brand rounded-pill px-3" for="tag-toggle-{{ $tagName }}">{{ $tagName }}</label>
+</div>
