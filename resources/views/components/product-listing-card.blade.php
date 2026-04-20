d3bc93bbde04cab42de73b740fdc28598cfc0ae1
b-chua-student <s2302677@usls.edu.ph>
2026-04-20 15:45:25 +0800

    feat(product listing card component): add data variables

--- resources/views/components/product-listing-card.blade.php
+++ resources/views/components/product-listing-card.blade.php
@@ -3,8 +3,8 @@
 
   </div>
   <div class="p-4 text-brand">
-    <p class="fs-6 m-0">Product Category</p>
-    <p class="fs-5 m-0">Product Name</p>
-    <p class="fw-bold fs-5 m-0">Product Price</p>
+    <p class="fs-6 m-0">{{ $productCategory }}</p>
+    <p class="fs-5 m-0">{{ $productName}}</p>
+    <p class="fw-bold fs-5 m-0">{{ $productPrice }}</p>
   </div>
 </div>
